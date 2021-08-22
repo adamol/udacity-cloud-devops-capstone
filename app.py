13caed6fb74f9f42f8d00304f27cbcb59fa8ddf4
Adam Olsson <adam.olsson@check24.de>
2021-08-22 10:16:34 +0200

    comment out lint step

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def hello_world():
     return resp
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=8080, debug=True) # specify port=80
