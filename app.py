627d779724ff007f0d411ddcba4e3be9292e87b5
Adam Olsson <adam.olsson@check24.de>
2021-08-22 09:57:05 +0200

    trigger build

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     output = cowsay.get_output_string('cow', 'Hello World')
+    resp = flask.Response(output)
 
-    output = output.replace('\n', '<br>')
+    resp.headers['Content-Type'] = 'text/plain'
 
-    return output
+    return resp
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
--- helm-stuff/README.txt
+++
@@ -1,1 +0,0 @@
-https://www.velotio.com/engineering-blog/cicd-for-kubernetes-using-circleci-and-helm
